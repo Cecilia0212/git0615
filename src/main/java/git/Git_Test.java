308ff5b5a4bcb872eac47e3ebce0d451d85405c2
Cecilia <Cecilia050122@163.com>
2022-06-15 20:50:08 +0800

    hf test commit

--- src/main/java/git/Git_Test.java
+++ src/main/java/git/Git_Test.java
@@ -6,5 +6,6 @@ public class Git_Test {
         System.out.println("git test2");
         System.out.println("git test3");
         System.out.println("hot-fix test1");
+        System.out.println("hot-fix test2");
     }
 }
