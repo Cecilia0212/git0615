3e6b07d4c6dd7ba3941edae52445da01af360ea8
Cecilia0212 <107489875+Cecilia0212@users.noreply.github.com>
2022-06-15 22:40:03 +0800

    pull test

--- src/main/java/git/Git_Test.java
+++ src/main/java/git/Git_Test.java
@@ -9,5 +9,6 @@ public class Git_Test {
         System.out.println("master test");
         System.out.println("hot-fix test2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
